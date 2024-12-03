5f1f3af0c89f74bc62f67fa41be8951d6c0355ad
pkritt <krittapat37819@gmail.com>
2024-12-03 16:34:41 +0700

    finish

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+char before(char x){
+if (x>='B' &&  x<='Z'){
+    x = x-1;
+}
+else if (x=='A'){x='Z';}
+else {x = '0';}    
+
+return x;
+}
+
+int main()
+{
+cout << before('A');
+cout << before('B');
+cout << before('Z');
+cout << before('[');
+
+}
