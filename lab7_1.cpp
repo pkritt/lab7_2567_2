a04d74a026b0d2afb39fbb562bfceaad4a540b98
pkritt <krittapat37819@gmail.com>
2024-12-03 14:45:47 +0700

    finish

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a  =  20, b = 30;
+    int c = 4;
+    double output;
+
+    output = double (a - b/double(c));
+    cout << "output: "<< output << "\n";
+    return 0;
+}
 
